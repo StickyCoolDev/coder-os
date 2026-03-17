b89fc55b665d73270bbcae130875ee146c1e097c
StickyCoolDev <hujifa.info@gmail.com>
2026-03-17 07:35:04 +0000

    Fix All ExpoFs and Http related issues

--- lib/expoFsAdapter.ts
+++ lib/expoFsAdapter.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { Buffer } from 'buffer';
 
 const ensureDir = async (path: string) => {
@@ -11,40 +11,51 @@ const ensureDir = async (path: string) => {
   }
 };
 
-export const expoFsAdapter = {
-  promises: {
-    readFile: async (path: string, options?: any) => {
-      const data = await FileSystem.readAsStringAsync(path, {
-        encoding: FileSystem.EncodingType.Base64,
-      });
-      const buffer = Buffer.from(data, 'base64');
-      return options?.encoding === 'utf8' ? buffer.toString('utf8') : buffer;
-    },
-    writeFile: async (path: string, data: any) => {
-      await ensureDir(path);
-      const content = Buffer.isBuffer(data) ? data.toString('base64') : Buffer.from(data).toString('base64');
-      await FileSystem.writeAsStringAsync(path, content, { encoding: FileSystem.EncodingType.Base64 });
-    },
-    mkdir: async (path: string) => {
-      await FileSystem.makeDirectoryAsync(path, { intermediates: true });
-    },
-    readdir: async (path: string) => {
-      return await FileSystem.readDirectoryAsync(path);
-    },
-    unlink: async (path: string) => {
-      await FileSystem.deleteAsync(path);
-    },
-    stat: async (path: string) => {
-      const info = await FileSystem.getInfoAsync(path);
-      if (!info.exists) throw { code: 'ENOENT' };
-      return {
-        size: info.size || 0,
-        mtimeMs: info.modificationTime * 1000,
-        isDirectory: () => info.isDirectory,
-        isFile: () => !info.isDirectory,
-      };
-    },
-    lstat: function(path: string) { return this.stat(path); }
+export const expoFsAdapter: any = {
+  readFile: async (path: string, options?: any) => {
+    const data = await FileSystem.readAsStringAsync(path, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    const buffer = Buffer.from(data, 'base64');
+    
+    return options?.encoding === 'utf8' ? buffer.toString('utf8') : new Uint8Array(buffer);
   },
+  writeFile: async (path: string, data: any) => {
+    await ensureDir(path);
+    const content = Buffer.isBuffer(data) ? data.toString('base64') : Buffer.from(data).toString('base64');
+    await FileSystem.writeAsStringAsync(path, content, { encoding: FileSystem.EncodingType.Base64 });
+  },
+  mkdir: async (path: string) => {
+    await FileSystem.makeDirectoryAsync(path, { intermediates: true });
+  },
+  rmdir: async (path: string) => {
+    await FileSystem.deleteAsync(path, { idempotent: true }); 
+  },
+  readdir: async (path: string) => {
+    return await FileSystem.readDirectoryAsync(path);
+  },
+  unlink: async (path: string) => {
+    await FileSystem.deleteAsync(path, { idempotent: true });
+  },
+  stat: async (path: string) => {
+    const info = await FileSystem.getInfoAsync(path);
+    if (!info.exists) {
+      const err = new Error('ENOENT');
+      (err as any).code = 'ENOENT';
+      throw err;
+    }
+    return {
+      size: info.size || 0,
+      mtimeMs: info.modificationTime * 1000,
+      isDirectory: () => info.isDirectory,
+      isFile: () => !info.isDirectory,
+      isSymbolicLink: () => false,
+    };
+  },
+  lstat: function (path: string) { return this.stat(path); },
+  readlink: async () => { throw new Error('Not implemented'); },
+  symlink: async () => { throw new Error('Not implemented'); },
 };
+
+expoFsAdapter.promises = expoFsAdapter;
 
