79e7f727bcf22aabacf88ecd059925f9cd3e31d5
StickyCoolDev <hujifa.info@gmail.com>
2026-03-21 18:06:29 +0000

    Add docs and Add git add in gitActions

--- lib/gitActions.ts
+++ lib/gitActions.ts
@@ -3,6 +3,10 @@ import { Paths, Directory } from 'expo-file-system'; // Added Directory
 import { gitHTTP } from '@/lib/gitHTTP';
 import { expoFsAdapter } from '@/lib/expoFsAdapter';
 
+/**
+ * Performes a git clone , similar to the command `git clone <url> <dir>`
+ * NOTE: does not do/allow for a shallow clone, could lead to OOM or OOS
+ */
 export async function performClone(remoteUrl: string, dirName?: string) {
   const folder = dirName ?? remoteUrl.split('/').pop()?.replace('.git', '') ?? 'new-project';
   
@@ -16,7 +20,7 @@ export async function performClone(remoteUrl: string, dirName?: string) {
     await git.clone({
       fs: expoFsAdapter,
       http: gitHTTP,
-      dir: repoDirUri, // Use the string URI here
+      dir: repoDirUri, 
       url: remoteUrl,
       singleBranch: true,
       onProgress: (progress) => {
@@ -24,11 +28,32 @@ export async function performClone(remoteUrl: string, dirName?: string) {
       }
     });
 
-    console.log('Clone successful!');
+    console.log('Clone successful');
     
     return { success: true, dir: repoDirUri, directoryObject: repoDirObj }; 
   } catch (err) {
     console.error('Clone failed:', err);
+    return { success: false, error: err };
+  }
+}
+/**
+ * Performes a git add, similar to the command `git add <>`
+ * Takes in a single path or a list of paths
+ */
+export async function performAdd(dir: string, filepath: string | string[]) {
+  try {
+    const filepaths = Array.isArray(filepath) ? filepath : [filepath];
+
+    for (const file of filepaths) {
+      await git.add({
+        fs: expoFsAdapter,
+        dir,
+        filepath: file,
+      });
+    }
+
+    return { success: true };
+  } catch (err) {
     return { success: false, error: err };
   }
 }
